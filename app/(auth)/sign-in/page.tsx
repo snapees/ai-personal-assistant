70d46057c6f85817dba85fff135e13c69d9ec09c
snapees <iamamitthakur2602@gmail.com>
2025-03-06 15:40:51 +0530

    `backend db setup and users details saved through useContext`

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,22 +1,35 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { AuthContext } from "@/context/AuthContext";
+import { api } from "@/convex/_generated/api";
 import { GetAuthUserData } from "@/services/GlobalApi";
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import { useMutation } from "convex/react";
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 
 function Signin() {
+  const CreateUser = useMutation(api.users.CreateUser);
+  const { user, setUSer } = useContext(AuthContext);
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       if (typeof window !== undefined) {
         localStorage.setItem("user_token", tokenResponse.access_token);
       }
 
-      const user = GetAuthUserData(tokenResponse.access_token);
+      const user = await GetAuthUserData(tokenResponse.access_token);
 
       console.log("user=>", user);
+      // save user information to the database
+      const result = await CreateUser({
+        name: user?.name,
+        email: user?.email,
+        picture: user.picture,
+      });
+      console.log("result--", result);
+      setUSer(result);
     },
     onError: (errorResponse) => console.log(errorResponse),
   });
