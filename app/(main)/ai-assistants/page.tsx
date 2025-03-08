eaaf4f09f1deab7a6add6ca5e09d6d711f89495d
snapees <iamamitthakur2602@gmail.com>
2025-03-08 20:18:29 +0530

    `feat: Added motion library and updated dependencies for animation features`

--- app/(main)/ai-assistants/page.tsx
+++ app/(main)/ai-assistants/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { BlurFade } from "@/components/magicui/blur-fade";
+import { RainbowButton } from "@/components/magicui/rainbow-button";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import AiAssistantsList from "@/services/AiAssistantsList";
@@ -42,40 +44,50 @@ function AIAssistants() {
     <div className="px-10 mt-20 md:px-28 lg:px-36 xl:px-48">
       <div className="flex justify-between items-center">
         <div>
-          <h2 className="text-3xl font-bold">
-            Welcome to the World of AI Assistants 🤖
-          </h2>
-          <p className="text-xl mt-2">
-            Choose your AI Campanion to Simplify Your Task 🚀
-          </p>
+          <BlurFade delay={0.25} inView>
+            <h2 className="text-3xl font-bold">
+              Welcome to the World of AI Assistants 🤖
+            </h2>
+          </BlurFade>
+
+          <BlurFade delay={0.25 * 2} inView>
+            <p className="text-xl mt-2">
+              Choose your AI Campanion to Simplify Your Task 🚀
+            </p>
+          </BlurFade>
         </div>
-        <Button>Continue</Button>
+        {/* <RainbowButton>Continue</RainbowButton> */}
+        <Button disabled={selectedAssistant.length === 0}>Continue</Button>
       </div>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mt-5">
         {AiAssistantsList.map((assistant, index) => (
-          <div
-            className="hover:border p-3 rounded-xl hover:scale-105 transition-all ease-in-out cursor-pointer relative"
-            key={index}
-            onClick={() => onSelect(assistant)}
-          >
-            <Checkbox
-              className="absolute m-2 border-black"
-              checked={isAssistantSelected(assistant)}
-            />
+          <BlurFade key={assistant.image} delay={0.25 + index * 0.05} inView>
+            <div
+              className="hover:border p-3 rounded-xl hover:scale-105 transition-all ease-in-out cursor-pointer relative"
+              key={index}
+              onClick={() => onSelect(assistant)}
+            >
+              <Checkbox
+                className="absolute m-2 border-black"
+                checked={isAssistantSelected(assistant)}
+              />
 
-            <Image
-              src={assistant.image}
-              alt={assistant.title}
-              width={600}
-              height={600}
-              className="rounded-xl w-full h-[200px] object-cover"
-            />
-            <h2 className="text-center font-bold text-lg">{assistant.name}</h2>
-            <h2 className="text-center text-gray-600 dark:text-gray-300">
-              {assistant.title}
-            </h2>
-          </div>
+              <Image
+                src={assistant.image}
+                alt={assistant.title}
+                width={600}
+                height={600}
+                className="rounded-xl w-full h-[200px] object-cover"
+              />
+              <h2 className="text-center font-bold text-lg">
+                {assistant.name}
+              </h2>
+              <h2 className="text-center text-gray-600 dark:text-gray-300">
+                {assistant.title}
+              </h2>
+            </div>
+          </BlurFade>
         ))}
       </div>
     </div>
